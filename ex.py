5756f7bce71447c98c4e2a6cc6cd27c2020aed10
ordenmeny <65502313+ordenmeny@users.noreply.github.com>
2022-09-11 12:50:55 +0500

    1, N

---
+++ ex.py
@@ -0,0 +1,21 @@
+from time import time
+from sys import getsizeof
+
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        st = time()
+        res = func(*args, **kwargs)
+        et = time()
+        print(f'Result: {res}, Time: {et - st}, Memory: {getsizeof(res)}')
+
+    return wrapper
+
+
+@decorator
+def func(N):
+    N += 1
+    return sum(range(1, N))
+
+
+func(10000000)
