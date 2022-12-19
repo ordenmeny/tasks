b0163e84241760c65353d7405e60a75b7e1a12f4
ordenmeny <65502313+ordenmeny@users.noreply.github.com>
2022-12-19 19:55:08 +0500

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,20 +1,28 @@
-from time import time
-from sys import getsizeof
+#  Создать список из n чисел.
+#  Найти количество пар, сумма которых простое число и минимальную такую сумму. Ответ перевести в семеричную С/Сч
+from random import randint
+
+lst = [randint(1, 100) for i in range(1, 100)]
 
 
-def decorator(func):
-    def wrapper(*args, **kwargs):
-        st = time()
-        res = func(*args, **kwargs)
-        et = time()
-        print(f'Result: {res}, Time: {et - st}, Memory: {getsizeof(res)}')
+def func(enum):
+    sum_s = lst[enum[0]] + lst[enum[0] + 1]
+    if sum_s == 2:
+        return lst[enum[0]], lst[enum[0] + 1]
+    if not (any(map(lambda x: sum_s % x == 0, range(2, sum_s)))):  # проверяет, простое ли число
+        return lst[enum[0]], lst[enum[0] + 1]  # если да, то возвращает элементы по индексу списка lst
+    return None
 
-    return wrapper
+
+def rotate(n):
+    st = ''
+
+    while n // 7 != 0:
+        st = str(n % 7) + st
+        st = str(n // 7) + st
+        n //= 7
+    return st if st != '' else n
 
 
-@decorator
-def func(N):
-    N += 1
-    return sum(range(1, N))
-
-func(10000000)
+res = rotate(min(map(lambda x: sum(x), filter(lambda x: x, map(func, enumerate(lst[:-1]))))))
+print(res)
